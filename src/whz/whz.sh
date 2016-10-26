bd5f4037f621b1ee4998f4466b620821b898e801
ben-arnold <benarnold@berkeley.edu>
2016-10-26 10:49:52 -0700

    Added WHZ analysis scripts

---
+++ src/whz/whz.sh
@@ -0,0 +1,10 @@
+
+
+R CMD BATCH whz-unadj-t1.R 
+R CMD BATCH whz-unadj-t2.R 
+R CMD BATCH whz-unadj-permute.R 
+
+R CMD BATCH whz-adj-t1.R 
+R CMD BATCH whz-adj-t2.R 
+R CMD BATCH whz-adj-permute.R 
+
