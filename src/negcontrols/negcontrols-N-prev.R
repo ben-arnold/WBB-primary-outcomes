182ab58a4c705bce6ae8c492739bd1cc26c1a8f2
ben-arnold <benarnold@berkeley.edu>
2016-10-26 10:51:46 -0700

    Added negative control analysis scripts

---
+++ src/negcontrols/negcontrols-N-prev.R
@@ -0,0 +1,173 @@
+
+#---------------------------------------
+# negcontrols-N-prev.R
+#
+# ben arnold (benarnold@berkeley.edu)
+#
+# calculate the number of bruise
+# cases and the prevalence by
+# survey round and treatment arm
+#---------------------------------------
+
+#---------------------------------------
+# input files:
+#	washb-bangladesh-diar.csv
+#
+# output files:
+#	bangladesh-negcontrols-N-prev-ben.RData
+#
+#---------------------------------------
+
+
+#---------------------------------------
+# preamble
+#---------------------------------------
+rm(list=ls())
+library(tmle)
+
+# source the base functions
+source("~/WBBpa/src/basefns/washb-base-functions.R")
+
+
+#---------------------------------------
+# Load the analysis dataset
+#---------------------------------------
+
+d <- read.csv("~/dropbox/wbb-primary-analysis/data/final/ben/washb-bangladesh-diar.csv")
+
+#---------------------------------------
+# Exclude:
+# * siblings who were born after enrollment
+# * siblings who were >36 mos at enrollment
+# * children with missing outcome data
+#---------------------------------------
+
+table(d$svy,d$sibnewbirth)
+table(d$svy,d$gt36mos)
+table(d$svy,is.na(d$bruise7d))
+
+
+table(d$sibnewbirth)
+ad <- subset(d,sibnewbirth==0)
+dim(ad)
+
+table(ad$gt36mos)
+ad <- subset(ad,gt36mos==0)
+dim(ad)
+
+table(is.na(ad$bruise7d))
+ad <- subset(ad,!is.na(ad$bruise7d))
+
+
+# re-order the tr factor for convenience
+ad$tr <- factor(ad$tr,levels=c("Control","Water","Sanitation","Handwashing","WSH","Nutrition","Nutrition + WSH"))
+
+#---------------------------------------
+# Calculate N and 7-day diarrhea cases
+# by survey round and by treatment arm
+#---------------------------------------
+
+Nchild <- tapply(ad$bruise7d,list(ad$svy,ad$tr),function(x) length(x))
+bruise7d <- tapply(ad$bruise7d,list(ad$svy,ad$tr),function(x) sum(x))
+
+#---------------------------------------
+# calculate Ns and prevalence for
+# all of follow-up (surveys 1 and 2)
+#---------------------------------------
+Nchildfu <- tapply(ad$bruise7d[ad$svy==1|ad$svy==2],ad$tr[ad$svy==1|ad$svy==2],function(x) length(x))
+bruise7dfu <- tapply(ad$bruise7d[ad$svy==1|ad$svy==2],ad$tr[ad$svy==1|ad$svy==2],function(x) sum(x))
+
+
+#---------------------------------------
+# Create randomization block indicators
+# (need to condition on these for 
+# clusters to be indepdent)  The tmle()
+# function can't handle factors, so this
+# is necessary
+#
+# NOTE: tmle() returns identical CIs
+# irrespective of whether we include
+# these indicators
+# might want to double-check the
+# estimates using Stata as well?
+#---------------------------------------
+
+# create indicators for the randomization strata
+blocks <- model.matrix(~as.factor(ad$block))[,-c(1)]
+colnames(blocks) <- paste("block",2:90,sep="")
+
+#---------------------------------------
+# Calculate unadjusted prevalences
+# and 95% CIs by arm
+#---------------------------------------
+
+# Calculate means and influence-curve based 95% CIs by survey round
+set.seed(12345)
+arms <- levels(ad$tr)
+mu0 <- sapply(arms,tmle.mean.est,Y=ad$bruise7d,tr=ad$tr,svy=ad$svy,id=ad$clusterid,s=0)
+mu1 <- sapply(arms,tmle.mean.est,Y=ad$bruise7d,tr=ad$tr,svy=ad$svy,id=ad$clusterid,s=1)
+mu2 <- sapply(arms,tmle.mean.est,Y=ad$bruise7d,tr=ad$tr,svy=ad$svy,id=ad$clusterid,s=2)
+
+# compute means by all follow-up rounds
+ad$svy2 <- ifelse(ad$svy==1|ad$svy==2,1,0)
+mu12 <- sapply(arms,tmle.mean.est,Y=ad$bruise7d,tr=ad$tr,svy=ad$svy2,id=ad$clusterid,s=1)
+
+#---------------------------------------
+# Create final objects
+#---------------------------------------
+
+# condense results into pre-specfied objects
+bruise_t0_n <- cbind(Nchild[1,],bruise7d[1,])
+bruise_t1_n <- cbind(Nchild[2,],bruise7d[2,])
+bruise_t2_n <- cbind(Nchild[3,],bruise7d[3,])
+bruise_t12_n <- cbind(Nchildfu,bruise7dfu)
+colnames(bruise_t0_n) <- colnames(bruise_t1_n) <-  colnames(bruise_t2_n) <- colnames(bruise_t12_n) <- c("N","n")
+
+bruise_t0_prev <- t(mu0)
+bruise_t1_prev <- t(mu1)
+bruise_t2_prev <- t(mu2)
+bruise_t12_prev <- t(mu12)
+
+
+
+#---------------------------------------
+# Internal consistency check:
+# the n/N and tmle prevalence estimates
+# should be the same
+#---------------------------------------
+round(cbind(bruise_t0_n[,2]/bruise_t0_n[,1],bruise_t0_prev[,1]),4)
+round(cbind(bruise_t1_n[,2]/bruise_t1_n[,1],bruise_t1_prev[,1]),4)
+round(cbind(bruise_t2_n[,2]/bruise_t2_n[,1],bruise_t2_prev[,1]),4)
+
+round(cbind(bruise_t12_n[,2]/bruise_t12_n[,1],bruise_t12_prev[,1]),4)
+
+#---------------------------------------
+# Print and save results
+#---------------------------------------
+bruise_t0_n
+bruise_t1_n
+bruise_t2_n
+bruise_t12_n
+round(bruise_t0_prev,4)
+round(bruise_t1_prev,4)
+round(bruise_t2_prev,4)
+round(bruise_t12_prev,4)
+
+# add 'b' suffix for comparison w/ jade
+bruise_t0_n_b <- bruise_t0_n
+bruise_t1_n_b <- bruise_t1_n
+bruise_t2_n_b <- bruise_t2_n
+bruise_t12_n_b <- bruise_t12_n
+bruise_t0_prev_b <- bruise_t0_prev
+bruise_t1_prev_b <- bruise_t1_prev
+bruise_t2_prev_b <- bruise_t2_prev
+bruise_t12_prev_b <- bruise_t12_prev
+
+
+save(bruise_t0_n_b,bruise_t1_n_b,bruise_t2_n_b,bruise_t12_n_b,bruise_t0_prev_b,bruise_t1_prev_b,bruise_t2_prev_b,bruise_t12_prev_b,file="~/dropbox/wbb-primary-analysis/results/raw/ben/bangladesh-bruise-N-prev-ben.RData")
+
+
+
+
+
+	
---
+++ src/negcontrols/negcontrols-figure.R
@@ -0,0 +1,114 @@
+# --------------------------------------
+# negcontrols-figure.R
+# ben arnold (benarnold@berkeley.edu)
+#
+# description:
+# plot the WASH B bangladesh negative control
+# results
+#
+#
+# --------------------------------------
+
+# --------------------------------------
+# input files:
+#	bangladesh-diar-N-prev.RData
+#   bangladesh-diar-unadj.RData
+#
+# output files:
+#	  bangladesh-bruise.pdf
+# --------------------------------------
+
+# --------------------------------------
+# preamble
+# --------------------------------------
+
+rm(list=ls())
+library(RColorBrewer)
+library(scales)
+
+# --------------------------------------
+# load the analysis output files
+# --------------------------------------
+
+load('~/Dropbox/wbb-primary-analysis/results/raw/ben/bangladesh-bruise-N-prev-ben.RData')
+load('~/Dropbox/wbb-primary-analysis/results/raw/ben/bangladesh-bruise-unadj-ben.RData')
+
+# --------------------------------------
+# format the objects for plotting
+# --------------------------------------
+
+glab <- c("Control\n","Water\n","Sanitation\n","Handwashing\n","Combined\nWSH","Nutrition\n","Combined\nNutrion+WSH")
+glab2 <- c("C","W","S","H","WSH","N","WSHN")
+
+t0n <- bruise_t0_n_b
+t1n <- bruise_t1_n_b
+t0p <- bruise_t0_prev_b
+t1p <- bruise_t1_prev_b
+fuPrev <- bruise_t12_prev_b
+
+h1pr <- bruise_h1_pr_unadj_b
+h2pr <- bruise_h2_pr_unadj_b
+
+
+pdf("~/dropbox/wbb-primary-analysis/results/figs/bangladesh-bruise.pdf",width=10,height=4)
+
+# cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# cols <- c("gray30",cbPalette[c(2:4,6:8)])
+# brighter color blind palette:  https://personal.sron.nl/~pault/ 
+cblack <- "#000004FF"
+cblue <- "#3366AA"
+cteal <- "#11AA99"
+cgreen <- "#66AA55"
+cchartr <- "#CCCC55"
+cmagent <- "#992288"
+cred <- "#EE3333"
+corange <- "#EEA722"
+cyellow <- "#FFEE33"
+cgrey <- "#777777"
+cols=c(cblack,cblue,cteal,cgreen,corange,cred,cmagent)
+
+ytics <- seq(0,7,by=1)
+op <- par(mar=c(1,9,9,0)+0.1,xpd=TRUE)
+# set up an empty plot
+MidPts <- barplot(1:7,names.arg=NA,border=NA,col=NA,
+	ylim=range(ytics),ylab="",yaxt="n",
+	las=1,bty="n"
+	)
+	segments(x0=0,x1=max(MidPts+0.5),y0=ytics,lty=2,lwd=1,col="gray80")
+	axis(2,at=ytics,las=1)
+	mtext("Bruising/\nAbrasion\nPrevalence\nDuring\nFollow-up\n(%)",side=2,line=3,las=1)
+
+	# plot estimates
+	arrows(x0=MidPts, y0=fuPrev[,2]*100, y1=fuPrev[,3]*100, col=cols,lwd=2,length=0.05,angle=90,code=3)
+	points(MidPts,fuPrev[,1]*100,pch=21,cex=2,lwd=2,col=cols,bg="white")
+	points(MidPts,fuPrev[,1]*100,pch=21,cex=2,lwd=0,col=cols,bg=alpha(cols,alpha=0.5))
+	text(x=MidPts+0.05,y=fuPrev[,1]*100,sprintf("%1.1f",fuPrev[,1]*100),pos=4,cex=1,col=cols,font=1)
+	
+	# print header and footer labels
+	mtext(glab,at=MidPts,side=3,line=6,col=cols,font=1  )
+	hx <- MidPts[1]-0.5
+	prform <- function(pr,lb,ub) {
+		paste(sprintf("%1.2f",pr)," (",sprintf("%1.2f",lb),", ",sprintf("%1.2f",ub),")",sep="")
+	}
+	
+	# print header table - PRs for H1	
+	mtext("Prevalence Ratio (95% CI)",side=3,line=5.5,at=hx,adj=1,cex=1)
+	mtext("Intervention v. Control",side=3,line=4.5,at=hx,adj=1,cex=0.8,col="gray30")
+	mtext(c("ref",prform(h1pr[,1],h1pr[,2],h1pr[,3])),side=3,line=4.5,at=MidPts,cex=0.8,col="gray30")
+	
+	# print header table - PRs for H2a - c
+	mtext("WSH v. W",side=3,line=3,at=hx,adj=1,cex=0.8,col="gray30")
+	mtext(c("ref",prform(h2pr[1,1],h2pr[1,2],h2pr[1,3])),side=3,line=3,at=MidPts[c(2,5)],cex=0.8,col=c(cols[2],"gray30"))
+	
+	mtext("WSH v. S",side=3,line=2,at=hx,adj=1,cex=0.8,col="gray30")
+	mtext(c("ref",prform(h2pr[2,1],h2pr[2,2],h2pr[2,3])),side=3,line=2,at=MidPts[c(3,5)],cex=0.8,col=c(cols[3],"gray30"))
+	
+	mtext("WSH v. H",side=3,line=1,at=hx,adj=1,cex=0.8,col="gray30")
+	mtext(c("ref",prform(h2pr[3,1],h2pr[3,2],h2pr[3,3])),side=3,line=1,at=MidPts[c(4,5)],cex=0.8,col=c(cols[4],"gray30"))
+	
+par(op)
+dev.off()
+
+
+
+
