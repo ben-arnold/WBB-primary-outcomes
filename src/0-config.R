6f32b277391a0e0f2c8c9579c959f60be91b4a72
Ruwan23 <51462111+Ruwan23@users.noreply.github.com>
2020-02-22 13:02:42 -0800

    to remove burden from user of individually installing packages, package installation is now checked; if not installed, is installed and loaded; if installed, is loaded

--- src/0-config.R
+++ src/0-config.R
@@ -1,17 +1,106 @@
 rm(list=ls())
-library(plyr)
-library(dplyr)
-library(coin)
-library(tidyverse)
-library(here)
-library(metafor)
-library(coin)
-library(metafor)
-library(tmle)
-library(SuperLearner)
-library(MASS)
-library(lmtest)
-library(sandwich)
-library(xtable)
-library(scales)
-library(RColorBrewer)
+
+if(!require(here)){
+  install.packages("here")
+  library(here)
+} else{
+  library(here)
+}
+
+if(!require(plyr)){
+  install.packages("plyr")
+  library(plyr)
+} else{
+  library(plyr)
+}
+
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library(dplyr)
+} else{
+  library(dplyr)
+}
+
+if(!require(coin)){
+  install.packages("coin")
+  library(coin)
+} else{
+  library(coin)
+}
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+} else{
+  library(tidyverse)
+}
+
+if(!require(metafor)){
+  install.packages("metafor")
+  library(metafor)
+} else{
+  library(metafor)
+}
+
+if(!require(tmle)){
+  install.packages("tmle")
+  library(tmle)
+} else{
+  library(tmle)
+}
+
+if(!require(SuperLearner)){
+  install.packages("SuperLearner")
+  library(SuperLearner)
+} else{
+  library(SuperLearner)
+}
+
+if(!require(MASS)){
+  install.packages("MASS")
+  library(MASS)
+} else{
+  library(MASS)
+}
+
+if(!require(lmtest)){
+  install.packages("lmtest")
+  library(lmtest)
+} else{
+  library(lmtest)
+}
+
+if(!require(sandwich)){
+  install.packages("sandwich")
+  library(sandwich)
+} else{
+  library(sandwich)
+}
+
+if(!require(xtable)){
+  install.packages("xtable")
+  library(xtable)
+} else{
+  library(xtable)
+}
+
+if(!require(scales)){
+  install.packages("scales")
+  library(scales)
+} else{
+  library(scales)
+}
+
+if(!require(RColorBrewer)){
+  install.packages("RColorBrewer")
+  library(RColorBrewer)
+} else{
+  library(RColorBrewer)
+}
+
+if(!require(lubridate)){
+  install.packages("lubridate")
+  library(lubridate)
+} else{
+  library(lubridate)
+}
